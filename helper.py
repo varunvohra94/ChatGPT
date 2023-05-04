4d6bcdaeeb61c2471840aea4e1ae943378217fb1
varunvohra94 <varun.vohra94@gmail.com>
2023-05-03 22:42:12 -0400

    changes

--- helper.py
+++ helper.py
@@ -1,17 +1,17 @@
 import os
 import openai
-from settings import CHAT_GPT_MODEL, TEMPERATURE
+from settings import CHAT_GPT_MODEL
 
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 
 CHAT_GPT_MODEL = "gpt-3.5-turbo"
 
-def get_completion(prompt, model=CHAT_GPT_MODEL):
+def get_completion(prompt, model=CHAT_GPT_MODEL,temperature=0):
     messages = [{"role": "user", "content": prompt}]
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=TEMPERATURE, # this is the degree of randomness of the model's output
+        temperature=temperature, # this is the degree of randomness of the model's output
     )
     return response.choices[0].message["content"]
 
@@ -19,7 +19,7 @@ def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0)
     response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
-        temperature=TEMPERATURE, # this is the degree of randomness of the model's output
+        temperature=temperature, # this is the degree of randomness of the model's output
     )
 #     print(str(response.choices[0].message))
     return response.choices[0].message["content"]
--- settings.py
+++ settings.py
@@ -3,4 +3,3 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
 CHAT_GPT_MODEL="gpt-3.5-turbo"
-TEMPERATURE=0
